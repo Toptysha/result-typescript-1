9804fd4597d86b0252ee29df359fdf48b1f8d416
Vladislav Baranov <toptun113@gmail.com>
2024-05-11 21:38:26 +0300

    some changes

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var totalPrice = function (_a) {
-    var price = _a.price, discount = _a.discount, isInstallment = _a.isInstallment, months = _a.months;
-    if (isInstallment) {
-        return (price - (price * (discount / 100))) / months;
-    }
-    return (price - (price * (discount / 100)));
+    var price = _a.price, _b = _a.discount, discount = _b === void 0 ? 0 : _b, _c = _a.isInstallment, isInstallment = _c === void 0 ? false : _c, _d = _a.months, months = _d === void 0 ? 12 : _d;
+    var resCount = price - (price * (discount / 100));
+    var result = isInstallment ? resCount / months : resCount;
+    return Math.round(result);
 };
 var price = totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 12 });
 console.log(price); // 6250
--- index.ts
+++ index.ts
@@ -1,15 +1,14 @@
 interface TotalPrice {
     price: number, 
-    discount: number, 
-    isInstallment: boolean, 
-    months: number
+    discount?: number, 
+    isInstallment?: boolean, 
+    months?: number
 }
 
-const totalPrice = ({ price, discount, isInstallment, months }: TotalPrice): number => {
-    if (isInstallment) {
-        return (price - (price * (discount / 100))) / months;
-    }
-    return (price - (price * (discount / 100)))
+const totalPrice = ({ price, discount = 0, isInstallment = false, months = 12 }: TotalPrice): number => {
+    let resCount = price - (price * (discount / 100))
+    let result = isInstallment ? resCount / months : resCount
+    return Math.round(result)
 };
 
 const price = totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 12 });
